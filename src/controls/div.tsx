b7444910e94dd5682f014d9639a97cccbc1d9aaf
Gary Zhang <gzhang@primeobjects.com>
2022-03-30 22:32:24 -0700

    removeProps in Div control

--- src/controls/div.tsx
+++ src/controls/div.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { cloneDeep, each, isArray, isNil } from "lodash";
+import React, {useEffect, useState} from "react";
+
 const Div = (props: Record<string, any>) => {
-    return <div {...props}>
+
+    const [params, setParams] = useState<Record<string,any>|null>(null);
+    const show = !isNil(params);
+
+    useEffect(()=>{
+        const newParams = cloneDeep(props);
+        if (isArray(props.removeProps))
+        {
+            each(props.removeProps, (rp:string)=>{
+                delete newParams[rp];
+            });
+            delete newParams.removeProps;
+        }
+        setParams(newParams);
+    },[]);
+
+    return show?<div {...params}>
         {props.children}
-    </div>;
+    </div>:<></>
 };
 
 export default Div;
